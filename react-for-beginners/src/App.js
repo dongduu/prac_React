725250765887ef46dce5e1c34983353c7fbd1db7
dongduu <4012popo@naver.com>
2022-06-13 19:04:34 +0900

    feat: cleanup function

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,40 +1,30 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+
+  // function byeFn() {
+  //   console.log("destroyed :(");
+  // }
+  // function hiFn() {
+  //   console.log("created :)");
+  //   return byeFn;
+  // }
+  // useEffect(hiFn, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter','keyword' changes");
-  }, [counter, keyword]);
-
-  // useEffect(() => {
-  //   console.log("CALL THE API...");
-  // }, []);
-  // useEffect(() => {
-  //   if (keyword !== "" && keyword.length > 5) {
-  //     console.log("SEARCH FOR", keyword);
-  //   }
-  // }, [keyword]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
