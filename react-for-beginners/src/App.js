947f1d673f083ba16c9477513e536683f1d65565
dongduu <4012popo@naver.com>
2022-06-15 15:20:02 +0900

    feat: movie app part one - fetch&then

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,57 +2,19 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setNeed(1);
-  };
-  const handleInput = (event) => {
-    setNeed(event.target.value);
-  };
-
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=4000")
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    )
       .then((res) => res.json())
       .then((json) => {
-        setCoins(json);
+        setMovies(json.data.movies);
         setLoading(false);
       });
   }, []);
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <>
-          <select onChange={onChange}>
-            <option>Select Coin!</option>
-            {coins.map((coin) => (
-              <option key={coin.id} value={coin.quotes.USD.price}>
-                {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-        </>
-      )}
-      <h2>Please enter the amount</h2>
-      <div>
-        <input
-          type="number"
-          value={need}
-          onChange={handleInput}
-          placeholder="dollor"
-        />
-        $
-      </div>
-      <h2>You can get {need / cost}</h2>
-    </div>
-  );
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
